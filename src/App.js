83386d927805de8280e2070cbf9661e18467e782
WH <herman.witold7@gmail.com>
2020-11-01 16:58:29 +0100

    first iteration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {SearchProducts} from "./SearchProduct";
+import {Navigation} from "./Navigation";
+import {NutritionVisualize} from "./NutritionVisualize";
 
-function App() {
+export const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <>
+      <Navigation/>
+      <SearchProducts/>
+      </>
+  )
+};
 
-export default App;
+
+
+
